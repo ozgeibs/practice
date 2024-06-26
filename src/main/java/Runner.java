9ca457d4aaf467711cfa9d797f508da5e2a5a1ed
ozgeibs <155913903+ozgeibs@users.noreply.github.com>
2024-06-26 08:44:20 +0300

    Update Runner.java

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,6 +2,6 @@ public class Runner {
     public static void main(String[] args) {
         // this is working space ...........
         System.out.println("Aaa");
-        //updated runner on githup repository
+        //updated runner on githup repository......
     }
 }

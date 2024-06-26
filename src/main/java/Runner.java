31243bea9b85aebaaa95df9f4d01d028dda6092f
Ozge <email>
2024-06-26 08:36:22 +0300

    V3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,5 +2,6 @@ public class Runner {
     public static void main(String[] args) {
         // this is working space ...........
         System.out.println("Aaa");
+        System.out.println("2");
     }
 }
